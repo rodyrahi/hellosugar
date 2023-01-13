11528cc699ac838adb7985c37c1823792c1ed1d5
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2023-01-13 21:23:16 +0530

    puh

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 
+const { localsName } = require("ejs");
 const express = require("express");
 const router = express.Router()
 
@@ -9,5 +10,34 @@ router.get("/", (req, res) => {
         isAuthenticated : req.oidc.isAuthenticated()
     });
   });
+
+
+  router.get("/basic", (req, res) => {
+    const plan= "Basic";
+    console.log(plan);
+    res.render("plans", { 
+        plan : plan,
+        isAuthenticated : req.oidc.isAuthenticated()
+    });
+  });
+  router.get("/pro", (req, res) => {
+    const plan= "Pro";
+    console.log(plan);
+    res.render("plans", { 
+        plan : plan,
+        isAuthenticated : req.oidc.isAuthenticated()
+    });
+  });
+
+  router.get("/enterprise", (req, res) => {
+    const plan= "Enterprise";
+    console.log(plan);
+    res.render("plans", { 
+        plan : plan,
+        isAuthenticated : req.oidc.isAuthenticated()
+    });
+  });
+
+
 
 module.exports = router

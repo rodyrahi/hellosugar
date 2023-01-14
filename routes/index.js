9352bdc93e6aba3c19b2fd1097f14e5abc2811ef
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2023-01-14 15:10:23 +0530

    kinda done

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-
+var con = require("../database.js");
 const { localsName } = require("ejs");
 const express = require("express");
 const router = express.Router()
@@ -7,6 +7,7 @@ router.get("/", (req, res) => {
     console.log(req.oidc.isAuthenticated())
     res.render("index", { 
         title: "My Express App" ,
+        user : JSON.stringify(req.oidc.user["nickname"], null, 2).replace(/"/g, ""),
         isAuthenticated : req.oidc.isAuthenticated()
     });
   });
@@ -17,6 +18,8 @@ router.get("/", (req, res) => {
     console.log(plan);
     res.render("plans", { 
         plan : plan,
+        user : JSON.stringify(req.oidc.user["nickname"], null, 2).replace(/"/g, ""),
+
         isAuthenticated : req.oidc.isAuthenticated()
     });
   });
@@ -25,6 +28,8 @@ router.get("/", (req, res) => {
     console.log(plan);
     res.render("plans", { 
         plan : plan,
+        user : JSON.stringify(req.oidc.user["nickname"], null, 2).replace(/"/g, ""),
+
         isAuthenticated : req.oidc.isAuthenticated()
     });
   });
@@ -34,10 +39,71 @@ router.get("/", (req, res) => {
     console.log(plan);
     res.render("plans", { 
         plan : plan,
+        user : JSON.stringify(req.oidc.user["nickname"], null, 2).replace(/"/g, ""),
         isAuthenticated : req.oidc.isAuthenticated()
     });
   });
 
 
+
+  router.post("/basic", (req, res) => {
+    console.log(req.body);
+  const {name , number , mail , business  , city } = req.body;
+  const type = "basic"
+  var sql =
+    "INSERT INTO client (name , number , mail , business , type , city) VALUES ?";
+  var values = [
+    [name , number , mail , business , type , city,],
+  ];
+  con.query(sql, [values], function (err, result) {
+    if (err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+  });
+   res.render('registered' , {
+    name: name,
+    user : JSON.stringify(req.oidc.user["nickname"], null, 2).replace(/"/g, ""),
+    isAuthenticated : req.oidc.isAuthenticated()
+   })
+});
+router.post("/pro", (req, res) => {
+  console.log(req.body);
+const {name , number , mail , business  , city } = req.body;
+const type = "pro"
+var sql =
+  "INSERT INTO client (name , number , mail , business , type , city) VALUES ?";
+var values = [
+  [name , number , mail , business , type , city,],
+];
+con.query(sql, [values], function (err, result) {
+  if (err) throw err;
+  console.log("Number of records inserted: " + result.affectedRows);
+});
+ res.render('registered' , {
+  name: name,
+  user : JSON.stringify(req.oidc.user["nickname"], null, 2).replace(/"/g, ""),
+  isAuthenticated : req.oidc.isAuthenticated()
+ })
+});
+
+
+router.post("/enterprise", (req, res) => {
+  console.log(req.body);
+const {name , number , mail , business  , city } = req.body;
+const type = "enterprise"
+var sql =
+  "INSERT INTO client (name , number , mail , business , type , city) VALUES ?";
+var values = [
+  [name , number , mail , business , type , city,],
+];
+con.query(sql, [values], function (err, result) {
+  if (err) throw err;
+  console.log("Number of records inserted: " + result.affectedRows);
+});
+ res.render('registered' , {
+  name: name,
+  user : JSON.stringify(req.oidc.user["nickname"], null, 2).replace(/"/g, ""),
+  isAuthenticated : req.oidc.isAuthenticated()
+ })
+});
 
 module.exports = router

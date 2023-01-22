7f77dec6c90f0ede84d9ab71b37234f32b1dc233
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2023-01-22 23:08:33 +0530

    more fixing

--- routes/app.js
+++ routes/app.js
@@ -1,14 +1,14 @@
 var con = require("../apidatabase.js");
-const express = require('express');
+const express = require("express");
 var router = express.Router();
-const http = require('http');
+const http = require("http");
 var socketIO = require("socket.io");
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
-var bot_ready = false
+var bot_ready = false;
 
-var user = ""
+var user = "";
 
 //============================================================================================================
 
@@ -34,43 +34,41 @@ const client = new Client({
 
 client.initialize();
 
-
-if (io instanceof require('socket.io')) {
-  console.log('io is an instance of socket.io');
+if (io instanceof require("socket.io")) {
+  console.log("io is an instance of socket.io");
 } else {
-  console.log('io is not an instance of socket.io');
+  console.log("io is not an instance of socket.io");
 }
 
-
 router.get("/edit/:name", function (req, res, next) {
-  
-  con.query(`SELECT * FROM questions WHERE name ="${req.params.name}" `, (err, rows, fields) => {
-    if (err) {
-      console.error(err);
-    } else {
-
-      
-      console.log(rows);
-    }con.query(
-      "SELECT name, message FROM questions WHERE user=?",
-      ["raj"],
-      function (err, result, fields) {
-        res.render("api/edit_question", { 
-          data: rows , 
-          data2: result ,
-          user: JSON.stringify(req.oidc.user["nickname"], null, 2).replace(/"/g, "")
-        });
-
-
+  console.log(" this is running " + user);
+  con.query(
+    `SELECT * FROM questions WHERE name ="${req.params.name}" AND user='${user}' `,
+    (err, rows, fields) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(rows);
       }
-    );
-
-    
-  });
+      con.query(
+        "SELECT name, message FROM questions WHERE user=?",
+        [user],
+        function (err, result, fields) {
+          res.render("api/edit_question", {
+            data: rows,
+            data2: result,
+            user: JSON.stringify(req.oidc.user["nickname"], null, 2).replace(
+              /"/g,
+              ""
+            ),
+          });
+        }
+      );
+    }
+  );
 });
 
 router.get("/get", function (req, res, next) {
-  
   con.query("SELECT * FROM questions", (err, rows, fields) => {
     if (err) {
       console.error(err);
@@ -84,70 +82,75 @@ router.get("/get", function (req, res, next) {
 router.post("/edit/:name", function (req, res) {
   console.log("this");
 
-    console.log(req.body);
-    const {name,tittle,message,footer,op1,op2,op3,op1_q,op2_q, op3_q,isfirst,type} = req.body;
-    const nameq = req.params.name
-    
+  console.log(req.body);
+  const {
+    name,tittle,message,footer,op1,op2,op3,op1_q,op2_q,op3_q,isfirst,type,
+  } = req.body;
+  const nameq = req.params.name;
 
-    const query = `UPDATE questions SET name="${name}",tittle="${tittle}", message="${message}", footer="${footer}", op1="${op1}",op2="${op2}" ,op3 = "${op3}" ,op1_q = "${op1_q}" ,op2_q = "${op2_q}",op3_q ="${op3_q}",isfirst = "${isfirst}",type = "${type}"   WHERE name="${nameq}"`
-    // const id = req.params.id;
-    con.query(query,
-      (err, rows) => {
-        if (err) {
-          throw err;
-        } else {
-          console.log(rows );
-          res.redirect("/")
-        }
-      });
+  const query = `UPDATE questions SET name="${name}",tittle="${tittle}", message="${message}", footer="${footer}", op1="${op1}",op2="${op2}" ,op3 = "${op3}" ,op1_q = "${op1_q}" ,op2_q = "${op2_q}",op3_q ="${op3_q}",isfirst = "${isfirst}",type = "${type}"   WHERE name="${nameq}"`;
+  // const id = req.params.id;
+  con.query(query, (err, rows) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(rows);
+      res.redirect("/dashboard/"+user);
 
+    }
+  });
 });
 
 router.post("/add", function (req, res) {
-  
   let data = req.body;
-  insert_questions(data.name,data.question,data.question_title,data.question_footer,data.op1,data.op2,data.op3,data.op1_q,data.op2_q,data.op3_q,user,data.isfirst);
-  console.log(data);
+  insert_questions(
+    data.name,
+    data.question,
+    data.question_title,
+    data.question_footer,
+    data.op1,
+    data.op2,
+    data.op3,
+    data.op1_q,
+    data.op2_q,
+    data.op3_q,
+    user,
+    data.isfirst
+  );
+  console.log("this is runing"+data);
   res.redirect("/");
-
 });
 
 router.get("/", (req, res) => {
+  user = JSON.stringify(req.oidc.user["nickname"], null, 2).replace(/"/g, "");
 
-
-  user =  JSON.stringify(req.oidc.user["nickname"], null, 2).replace(/"/g, "")
-
-
-    user = JSON.stringify(req.oidc.user["nickname"], null, 2).replace(/"/g, "")
+  user = JSON.stringify(req.oidc.user["nickname"], null, 2).replace(/"/g, "");
   console.log(user);
 
-  var io = req.app.get('socketio');
+  var io = req.app.get("socketio");
 
-  io.on('connect', function() {
+  io.on("connect", function () {
     console.log("Connected to WS server");
     io.emit("ok", "ok");
     if (bot_ready) {
       io.emit("ready", "Hii bot is ready");
-    
     }
-    
+
     client.on("qr", (qr) => {
-      io.emit('qr', qr);
+      io.emit("qr", qr);
       console.log("QR RECEIVED", qr);
-    
     });
-    
+
     client.on("ready", () => {
-      bot_ready = true
+      bot_ready = true;
       io.emit("ready", "Hii bot is ready");
       console.log("Client is ready!");
     });
-  
   });
   // con.query(
   //   `SELECT name, message FROM questions WHERE user=${user}`,
   //   function (err, result, fields) {
-  //     res.render("api/api", { 
+  //     res.render("api/api", {
   //       data: result,
   //       user: JSON.stringify(req.oidc.user["nickname"], null, 2).replace(/"/g, "")
   //     });
@@ -156,29 +159,28 @@ router.get("/", (req, res) => {
     "SELECT name, message FROM questions WHERE user=?",
     [user],
     function (err, result, fields) {
-      res.render("api/api", { 
-        data: result ,
-        user: JSON.stringify(req.oidc.user["nickname"], null, 2).replace(/"/g, "")
+      res.render("api/api", {
+        data: result,
+        user: JSON.stringify(req.oidc.user["nickname"], null, 2).replace(
+          /"/g,
+          ""
+        ),
       });
     }
   );
-
-
 });
 router.post("/", (req, res) => {
-  
-  var io = req.app.get('socketio');
-  io.on('connect', function() {
+  var io = req.app.get("socketio");
+  io.on("connect", function () {
     console.log("Connected to WS server");
     io.emit("qr", "test2");
-  
   });
 });
 
 //==================================================================================================================
 
 function setlast_question(q) {
-  const sql = `UPDATE client SET lastq = ? WHERE name = 'raj'`;
+  const sql = `UPDATE client SET lastq = ? WHERE name = '${user}'`;
   con.query(sql, [q], (err, result) => {
     if (err) {
       throw err;
@@ -188,9 +190,7 @@ function setlast_question(q) {
 }
 function send_buttons(element, msg) {
   console.log("button is send");
-  const options = [{ body: element["op1"] },
-  { body: element["op2"] },
-  ];
+  const options = [{ body: element["op1"] }, { body: element["op2"] }];
   if (element["op3"] !== "") {
     options.push({ body: element["op3"] });
   }
@@ -206,37 +206,26 @@ function send_buttons(element, msg) {
 function send_list(element, msg) {
   console.log(element);
 
-  let list = []
+  let list = [];
 
-  let j = element["op3"]
-  list = j.split(',')
+  let j = element["op3"];
+  list = j.split(",");
   const productsList = new List(
     "Here's our list of products at 50% off",
     "View all products",
     [
       {
         title: "Products list",
-        rows: [
-          { id: "apple", title: "Apple" },
-        ],
+        rows: [{ id: "apple", title: "Apple" }],
       },
     ],
     "Please select a product"
   );
   client.sendMessage(msg.from, productsList);
-
-
-
-
 }
 function insert_number_in_db(number, m) {
-  var sql =
-    "INSERT INTO input_tb (number , input) VALUES ?";
-  var values = [
-    [
-      number, m
-    ],
-  ];
+  var sql = "INSERT INTO input_tb (number , input) VALUES ?";
+  var values = [[number, m]];
   con.query(sql, [values], function (err, result) {
     if (err) throw err;
     console.log("Number of records inserted: " + result.affectedRows);
@@ -248,74 +237,68 @@ async function send_input(element, msg) {
   client.on("message", async (msg) => {
     setlast_question(element["name"]);
 
-    client.sendMessage(msg.from, " value you stored:"+ msg.body);
-
-
+    client.sendMessage(msg.from, " value you stored:" + msg.body);
   });
-
 }
 async function next_message(q, msg) {
   con.query(
-    `SELECT * FROM questions WHERE name="${q}" AND user=${user}`,
+    `SELECT * FROM questions WHERE name="${q}" AND user='${user}'`,
     async function (err, element, fields) {
       console.log(element[0]["type"]);
       if (element[0]["type"] === "file") {
         const media = MessageMedia.fromFilePath(element[0]["message"]);
         client.sendMessage(msg.from, media);
-        console.log(element[0]['op1_q']);
-        next_message(element[0]['op1_q'], msg)
+        console.log(element[0]["op1_q"]);
+        next_message(element[0]["op1_q"], msg);
       } else {
-        send_buttons(element[0], msg)
+        send_buttons(element[0], msg);
       }
     }
   );
 }
 async function send_message(q, msg) {
-  con.query(
-    `SELECT * FROM questions WHERE name="${q}" AND user=${user}`,
-    async function (err, element, fields) {
 
+  con.query(
+    `SELECT * FROM questions WHERE name="${q}" AND user='${user}'`,
+    async function (err, element, fields) {
+      console.log("question types data" + element );
       if (element[0]["type"] === "file") {
-      const media = MessageMedia.fromFilePath(element[0]["message"]);
-      await client.sendMessage(msg.from, media);
-      console.log(element[0]['op1_q']);
-      next_message(element[0]['op1_q'], msg)
-    } else if (element[0]["type"] === "list") {
-      send_list(element[0], msg)
-    } else if (element[0]["type"] === "input") {
-      // await msg.reply('pong');
-      send_input(element[0], msg)
-    } else {
-      send_buttons(element[0], msg)
-    }
+        const media = MessageMedia.fromFilePath(element[0]["message"]);
+        await client.sendMessage(msg.from, media);
+        console.log(element[0]["op1_q"]);
+        next_message(element[0]["op1_q"], msg);
+      } else if (element[0]["type"] === "list") {
+        send_list(element[0], msg);
+      } else if (element[0]["type"] === "input") {
+        // await msg.reply('pong');
+        send_input(element[0], msg);
+      } else {
+        send_buttons(element[0], msg);
+      }
     }
   );
 }
 client.on("message", async (msg) => {
   console.log("MESSAGE RECEIVED", msg.body);
   let found_question = false;
-  var sql = `SELECT* FROM questions WHERE user=${user}`;
+  var sql = `SELECT* FROM questions WHERE user='${user}'`;
 
   con.query(sql, function (err, results) {
     if (err) throw err;
     results.forEach((element) => {
-
       if (element["op1"] === msg.body) {
         found_question = true;
         send_message(element["op1_q"], msg);
         return true;
-      }
-      else if (element["op2"] === msg.body) {
+      } else if (element["op2"] === msg.body) {
         found_question = true;
         send_message(element["op2_q"], msg);
         return true;
-      }
-      else if (element["op3"] === msg.body) {
+      } else if (element["op3"] === msg.body) {
         found_question = true;
         send_message(element["op3_q"], msg);
         return true;
-      }
-      else {
+      } else {
         // var sql =
         //   `SELECT lastq FROM client   WHERE name="raj"`;
         // con.query(sql, (err, result, fields) => {
@@ -326,44 +309,80 @@ client.on("message", async (msg) => {
         //     found_question = true;
         //   }
         //   });
-
       }
-    })
-
     });
 
 
-    if (!found_question) {
-      console.log("not found");
-      var sql =
-        "SELECT * FROM questions WHERE user = 'raj' AND isfirst = 'yes'";
+  if (!found_question) {
+    console.log("not found");
+    var sql = `SELECT * FROM questions WHERE user = "${user}" AND isfirst = 'yes'`;
+  
 
-      con.query(sql, function (err, results) {
-        if (err) {throw err;}
-        
-        if (results[0]["op3"] === "") {
-          let button = new Buttons(results[0]["message"],[{ body: results[0]["op1"] }, { body: results[0]["op2"] }],results[0]["tittle"],results[0]["footer"]);
-          setlast_question(results[0]["name"]);
-          client.sendMessage(msg.from, button);
-          console.log(results[0]["message"]);
-        } 
-        else {
-          let button = new Buttons(results[0]["message"],[{ body: results[0]["op1"] },{ body: results[0]["op2"] },{ body: results[0]["op3"] },],results[0]["tittle"],results[0]["footer"]);
-          setlast_question(element["name"]);
-          client.sendMessage(msg.from, button);
-        }
-      });
-    }
-       
-  });
+    con.query(sql, function (err, results) {
+      if (err) {
+        throw err;
+      }
+      console.log(results);
 
+      if (results[0]["op3"] === "") {
+        let button = new Buttons(
+          results[0]["message"],
+          [{ body: results[0]["op1"] }, { body: results[0]["op2"] }],
+          results[0]["tittle"],
+          results[0]["footer"]
+        );
+        setlast_question(results[0]["name"]);
+        client.sendMessage(msg.from, button);
+        console.log(results[0]["message"]);
+      } else {
+        let button = new Buttons(
+          results[0]["message"],
+          [
+            { body: results[0]["op1"] },
+            { body: results[0]["op2"] },
+            { body: results[0]["op3"] },
+          ],
+          results[0]["tittle"],
+          results[0]["footer"]
+        );
+        setlast_question(results[0]["name"]);
+        client.sendMessage(msg.from, button);
+      }
+    });
+  }
+});
+});
 
-function insert_questions( name,tittle,message,footer,op1,op2,op3,op1_q,op2_q,op3_q,user,isfirst) {
+function insert_questions(
+  name,
+  tittle,
+  message,
+  footer,
+  op1,
+  op2,
+  op3,
+  op1_q,
+  op2_q,
+  op3_q,
+  user,
+  isfirst
+) {
   var sql =
     "INSERT INTO questions (name , message , tittle ,footer , op1 , op2 , op3 , op1_q , op2_q , op3_q , user , isfirst ) VALUES ?";
   var values = [
     [
-      name,message,tittle,footer,op1,op2,op3,op1_q,op2_q,op3_q,user,isfirst,
+      name,
+      message,
+      tittle,
+      footer,
+      op1,
+      op2,
+      op3,
+      op1_q,
+      op2_q,
+      op3_q,
+      user,
+      isfirst,
     ],
   ];
   con.query(sql, [values], function (err, result) {
@@ -371,4 +390,4 @@ function insert_questions( name,tittle,message,footer,op1,op2,op3,op1_q,op2_q,op
     console.log("Number of records inserted: " + result.affectedRows);
   });
 }
-module.exports = router , client
+(module.exports = router), client;

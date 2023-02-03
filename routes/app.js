84529e7f033ef35fe7eafa0ffccd138b05320a4a
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2023-02-03 20:31:53 +0530

    trying nav

--- routes/app.js
+++ routes/app.js
@@ -25,18 +25,7 @@ const {
 var client = new Client({
   restartOnAuthFail: true,
   puppeteer: {
-    headless: true,
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-      "--disable-accelerated-2d-canvas",
-      "--no-first-run",
-      "--no-zygote",
-      "--single-process", // <- this one doesn't works in Windows
-      "--disable-gpu",
-      "--use-gl=egl",
-    ],
+    headless: true, args: [ '--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-accelerated-2d-canvas', '--no-first-run', '--no-zygote', '--single-process', '--disable-gpu', ],
   },
   authStrategy: new LocalAuth({
     clientId: user,
